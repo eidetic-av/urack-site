6abcb137f89f1890287ddd0d04ddf742e716bb00
Matthew Hughes <matt.hughes@live.com>
2020-07-23 18:56:33 +1000

    Created site generation setup + first page

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode
+  (org-hugo-default-static-subdirectory-for-externals . "urack/static/"))
+ ("org/"
+  (org-mode
+   (eval org-hugo-auto-export-mode))))
