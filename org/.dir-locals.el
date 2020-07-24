8b626ec78cb59c73c25fa21b995e85670efc8e44
Matthew Hughes <matt.hughes@live.com>
2020-07-24 14:47:05 +1000

    moved dir-locals.el

---
+++ org/.dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode
+  (org-hugo-default-static-subdirectory-for-externals . "urack/static/")
+  (eval org-hugo-auto-export-mode)))
+
+(eval (setq hugo-project-dir "/remote/micro/net/urack/hugo/"))
